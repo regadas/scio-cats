458652ca9ef37d3ccda154d19f65611f5cbd9c40
Scala Steward <me@scala-steward.org>
2020-03-30 22:29:52 +0200

    Update scio-core, scio-test to 0.8.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   val ScalaTestVersion = "3.1.1"
   val KindProjectorVersion = "0.11.0"
   val CatsVersion = "2.0.0"
-  val ScioVersion = "0.8.3"
+  val ScioVersion = "0.8.4"
 
   lazy val ScalaTest = "org.scalatest" %% "scalatest" % ScalaTestVersion
   lazy val CatsCore = "org.typelevel" %% "cats-core" % CatsVersion
