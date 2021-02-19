a8871f81c1a22b0f2f1ea1e8c102f11b6e1c5973
Scala Steward <me@scala-steward.org>
2021-02-19 17:37:32 +0100

    Update scalatest to 3.2.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val ScalaTestVersion = "3.2.3"
+  val ScalaTestVersion = "3.2.5"
   val KindProjectorVersion = "0.11.3"
   val CatsVersion = "2.4.1"
   val ScioVersion = "0.9.6"
