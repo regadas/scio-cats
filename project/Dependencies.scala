28c4116fa784283368a5ea51658edebbcd58e9f5
Scala Steward <me@scala-steward.org>
2021-05-15 12:54:14 +0200

    Update kind-projector to 0.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val ScalaTestVersion = "3.2.7"
-  val KindProjectorVersion = "0.11.3"
+  val KindProjectorVersion = "0.13.0"
   val CatsVersion = "2.4.2"
   val ScioVersion = "0.10.2"
 
